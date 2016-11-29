31e7112c971f76a5eab11dd0960c2af245ba5691
kasperfage <kasperfage@hotmail.com>
2016-11-29 11:26:05 +0100

    song data
    name, artist, genre, duration
    getters and setters for each of them

--- src/soundhive/SongFile.java
+++ src/soundhive/SongFile.java
@@ -10,5 +10,48 @@ package soundhive;
  * @author EdwinSilva
  */
 public class SongFile {
+    private String name;
+    private String artist;
+    private String genre;
+    private int duration;
+    
+    public SongFile(String name, String artist, String genre, int duration){
+        this.name = name;
+        this.artist = artist;
+        this.genre = genre;
+        this.duration = duration;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
     
 }
