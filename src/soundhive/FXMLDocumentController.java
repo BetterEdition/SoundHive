889da821cfc683122fc8ac8e0f59dec22291c960
robie sun <robie.sun.rs@gmail.com>
2016-11-29 11:15:19 +0100

    just user interface design

--- src/soundhive/FXMLDocumentController.java
+++ src/soundhive/FXMLDocumentController.java
@@ -10,29 +10,76 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 /**
+ * FXML Controller class
  *
- * @author Jesper Enemark
+ * @author robiesun
  */
-public class FXMLDocumentController implements Initializable
-{
-    
+public class FXMLDocumentController implements Initializable {
+
     @FXML
-    private Label label;
-    
+    private TextField textSearch;
     @FXML
-    private void handleButtonAction(ActionEvent event)
-    {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+    private TableView<String> songTableView;
+    @FXML
+    private ListView<String> listSong;
+    @FXML
+    private TableView<String> viewPlayList;
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
-    public void initialize(URL url, ResourceBundle rb)
-    {
+    public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void searchClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void newBtnSong(ActionEvent event) {
+    }
+
+    @FXML
+    private void editBtnSong(ActionEvent event) {
+    }
+
+    @FXML
+    private void deleteBtnSong(ActionEvent event) {
+    }
+
+    @FXML
+    private void closeBtnSong(ActionEvent event) {
+    }
+
+    @FXML
+    private void newBtnPlayList(ActionEvent event) {
+    }
+
+    @FXML
+    private void editbtnPlayList(ActionEvent event) {
+    }
+
+    @FXML
+    private void deleteBtnPlayList(ActionEvent event) {
+    }
+
+    @FXML
+    private void upBtnSongsOnPl(ActionEvent event) {
+    }
+
+    @FXML
+    private void downBtnSongsOnPl(ActionEvent event) {
+    }
+
+    @FXML
+    private void deleteBtnSongsOnPl(ActionEvent event) {
+    }
     
 }
